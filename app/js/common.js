3c3fe81a5c575d0ec57bf7a282891aadc88333e1
beast1 <beast.loby@gmail.com>
2018-03-18 12:09:53 +0200

    finish

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
 $(function() {
-
-	// Custom JS
-
+  function fixCatalog() {
+//    var window = document.querySelector("body")
+    var header = document.querySelector(".page-header");
+    var footer = document.querySelector(".page-footer");
+    var catalog = {};
+    catalog.elem = document.querySelector(".catalog-list");
+    catalog.MARGIN_TOP = 72;
+    catalog.MARGIN_BOTTOM = 80;
+    var newCatalogHeight = window.innerHeight - header.clientHeight - footer.clientHeight - catalog.MARGIN_TOP - catalog.MARGIN_BOTTOM - 20;
+    console.log(window.innerHeight, header.clientHeight, footer.clientHeight, catalog.MARGIN_TOP, catalog.MARGIN_BOTTOM, newCatalogHeight - 20);
+    catalog.elem.style.height = newCatalogHeight + "px";
+  }
+  fixCatalog();
 });
